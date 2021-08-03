3a2ddeec95d9226c567b04359821276b14c1ae87
marcioandrade <marcioandrade.dev@gmail.com>
2021-08-03 14:19:50 -0300

    scss global aplicado e head dinamico configurado

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,16 @@
-import styles from '../styles/styles.module.scss'
+import Head from 'next/head';
+import styles from '../styles/styles.module.scss';
 
 export default function Home() {
   return (
+    <>
+    <Head>
+      <title>Board - Organizando suas tarefas</title>
+    </Head>
     <div>
+      
       <h1 className={styles.title}>Primeiro Projeto com <span>NextJS</span></h1>
     </div>
+    </>
   );
 }
