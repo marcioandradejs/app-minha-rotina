1d2579de826836d33235d8cc411c5dfafdad6612
marcioandrade <marcioandrade.dev@gmail.com>
2021-08-04 17:06:09 -0300

    pagina home estatica utilizando um revalidate de 60min

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
 import styles from '../styles/styles.module.scss';
 
@@ -28,4 +29,15 @@ export default function Home() {
     </main>
     </>
   );
+}
+
+export const getStaticPorps: GetStaticProps = async () => {
+
+
+  return{
+    props: {
+
+    },
+    revalidate: 60 * 60 //atualiza a cada 60 minutos.
+  }
 }
