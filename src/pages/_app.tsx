2b60d0d204f6b299fdbc71eca099d818f0104470
marcioandrade <marcioandrade.dev@gmail.com>
2021-08-05 18:34:26 -0300

    pagamento via paypal configurado

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,22 @@
 import { AppProps } from 'next/app';
 import '../styles/global.scss';
 import { Header } from '../components/Header';
-
 import { Provider as NextAuthProvider } from 'next-auth/client'
+import { PayPalScriptProvider } from '@paypal/react-paypal-js';
+
+const initialOptions = {
+  "client-id": "AVOJVBDe9WtSDCBLkfCOM9b0uFBqqG2IcizY0tNhvgvFQDEPvmkNOLUi9r_6QBuaQfK5qzwBF36Lzhdj",
+  currency: "BRL",
+  intent: "capture",
+};
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <NextAuthProvider session={pageProps.session}>
-      <Header />
-      <Component {...pageProps} />;
+      <PayPalScriptProvider options={initialOptions}>
+        <Header />
+        <Component {...pageProps} />
+      </PayPalScriptProvider>
     </NextAuthProvider>
   )
 }
